61eff09ffe4b880a54270d584d22f727c903cfd7
AntoEGlez <marcoestrada11@hotmail.com>
2026-02-24 13:00:25 -0600

    Initial commit

---
+++ custom_addons/library_loans/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'Biblioteca: Préstamos',
+    'version': '1.0',
+    'category': 'Services',
+    'depends': ['library_books'], # IMPORTANTE: Dependes de tu compañero
+    'data': ['views/loan_views.xml'],
+    'installable': True,
+}
